1d3d5c5502f359fde1e68e585239b7da7d86195b
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-04-26 20:40:42 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,13 +1,16 @@
 import telebot
 from telebot import types
 import conexao_mongo
-import funcoes.buscas
-import funcoes.tratamentos
 from unidecode import unidecode
+import funcoes.buscas
+import funcoes.consultas_maps
+import funcoes.tratamentos
 import keys
+import funcoes
+api_key = keys.key 
 
-CHAVE_API = keys.CHAVE_API
-bot = telebot.TeleBot(CHAVE_API) #criação/conexão com a  chave api
+
+bot = telebot.TeleBot(keys.CHAVE_API) #criação/conexão com a  chave api
 
 # Função para lidar com o comando /start
 @bot.message_handler(commands=['start'])
@@ -35,7 +38,7 @@ def ask_location(message):
 @bot.message_handler(func=lambda message: message.text == 'Expor um problema que vi, mas não estou no local.')
 def ask_location_sem_local(message):
     user_data['state_localizacao'] = 'sem confirmacao'
-    bot.send_message(message.chat.id, "Por favor, envie uma imagem relacionada ao problema:")
+    bot.send_message(message.chat.id, "Por favor, envie a foto do problema:")
 
 @bot.message_handler(func=lambda message: message.text == 'visitar a plataforma, e ter um panorama geral!')
 def ask_location_sem_local(message):
@@ -49,9 +52,9 @@ def ask_location_sem_local(message):
 @bot.message_handler(content_types=['location'])
 def ask_image(message):
     user_data['state_localizacao'] = 'gps'
-    latitude, longitude = message.location.latitude, message.location.longitude
+    coordenadas = [message.location.latitude, message.location.longitude]
     user_data['localizacao'] = [message.location.latitude, message.location.longitude]
-    user_data['uf'] = funcoes.tratamentos.salvar_uf(latitude, longitude, CHAVE_API) #capturar estado
+    user_data['uf'] = funcoes.consultas_maps.salvar_uf(coordenadas) #capturar estado (uf)
     
     print(f"ESTADO -> {user_data['uf']}")
     print(user_data)    
@@ -75,68 +78,28 @@ def save_data(message):
 
 @bot.message_handler(func=lambda message: True and user_data['state_localizacao'] == 'sem confirmacao')
 def save_data_longe(message): 
-    if user_data['state_localizacao'] == 'sem confirmacao':
-        user_data['state_localizacao'] = 'confirmar estado'
-        user_data['descricao'] = message.text
-        print(user_data)
-        bot.send_message(message.chat.id, 'Digite o estado (UF)')
+    user_data['state_localizacao'] = 'confirmar localizacao'
+    user_data['descricao'] = message.text
+    print(user_data)
+    bot.send_message(message.chat.id, 'insira o endereço do local.')
+    bot.send_message(message.chat.id, 'diga o estado, cidade, bairro e rua. Não precisa ser na ordem!')
     
 
-@bot.message_handler(func=lambda message: True and user_data['state_localizacao'] == 'confirmar estado')
+@bot.message_handler(func=lambda message: True and user_data['state_localizacao'] == 'confirmar localizacao')
 def save_data_longe(message):
-        user_data['state_localizacao'] = 'confirmar cidade'
+        user_data['state_localizacao'] = 'manual'
         print(message.text)
-        uf = message.text.title()
-        uf = unidecode(uf)
-        uf_pesquisa = message.text.title().replace(" ", "_")
-        uf_pesquisa = unidecode(uf_pesquisa)
+
+        resultados = funcoes.consultas_maps.verificar_endereco(message.text)
+        user_data['localizacao'] = resultados
+        uf = funcoes.consultas_maps.salvar_uf(resultados)
         user_data['uf'] = uf
-        busca = funcoes.buscas.consultar_estado(uf_pesquisa)
-        bot.send_message(message.chat.id, f"Agora informe em qual cidade de {busca} você está.")
-        user_data['localizacao'] = busca
-        
-
-@bot.message_handler(func=lambda message: True and user_data['state_localizacao'] == 'confirmar cidade')
-def save_data_longe(message):
-        user_data['state_localizacao'] = 'confirmar bairro'
-        print(message.text)
-        cidade_pesquisa = message.text.title().replace(" ", "_")
-        cidade_pesquisa = unidecode(cidade_pesquisa)
-        user_data['cidade'] = message.text
-        busca = funcoes.buscas.consultar_cidade(cidade_pesquisa, user_data['localizacao'])
-        bot.send_message(message.chat.id, f"Agora informe em qual bairro de {busca} você está.")
-
-@bot.message_handler(func=lambda message: True and user_data['state_localizacao'] == 'confirmar bairro')
-def save_data_longe(message):
-    user_data['state_localizacao'] = 'confirmar rua'
-    user_data['bairro'] = message.text
-    bot.send_message(message.chat.id, f"Agora informe em qual rua de {message.text} você está.")
-
-
-@bot.message_handler(func=lambda message: True and user_data['state_localizacao'] == 'confirmar rua')
-def save_data_longe(message):
-    user_data['state_localizacao'] = 'manual'
-    user_data['rua'] = message.text
-    
-    estado = user_data['uf']
-    municipio = user_data['cidade']
-    bairro = user_data['bairro']
-    rua = user_data['rua']
-    print(estado, municipio, bairro, rua)
-
-    resultados = funcoes.tratamentos.verificar_endereco(estado, municipio, bairro, rua)
-    user_data['localizacao'] = resultados
-    
-    conexao_mongo.adicionar_dados(user_data, user_data['uf'].title())
-    del user_data['cidade']
-    del user_data['bairro']
-    del user_data['rua']
-
-    bot.send_message(message.chat.id, f"Tudo ok! Você pode ver por esse e outros locais em [link plataforma]!")
-
+        print(user_data)
+        conexao_mongo.adicionar_dados(user_data, user_data['uf'].title())
+        bot.send_message(message.chat.id, f"Tudo ok! Você pode ver por esse e outros locais em [link plataforma]!")
 
 bot.polling()
 
-#r### validar localizacao por texto pelo google maps.
+#r### validar localizacao por texto pelo google maps.               
 
 
