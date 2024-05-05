760b3d68b271bdab01029f820ee252078636a39c
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-05-05 16:38:05 -0300

    Update conexao_mongo.py

--- conexao_mongo.py
+++ conexao_mongo.py
@@ -1,20 +1,25 @@
 import pymongo
-import k
+from keys import STRING_CONNECTION
 
 
 # Conectar ao servidor MongoDB (por padrão, o MongoDB roda em localhost, porta 27017)
-client = pymongo.MongoClient(k.STRING_CONNECTION)
+client = pymongo.MongoClient(STRING_CONNECTION)
 
-# banco de dados insert
+# banco de dados
 unioes_federativas = client["unioes_federativas"]
-sao_paulo_collection = unioes_federativas["sao paulo"]
-
-#banco de dados para consulta
-dtb = client["dtb"]
-dtb_completo = dtb["dtb_completo"]
-
+# coleções
+sao_paulo_collection = unioes_federativas["sao_paulo"]
+parana_collection = unioes_federativas['parana']
+rio_grande_do_sul_collection = unioes_federativas['rio_grande_do_sul']
+santa_catarina_collection = unioes_federativas['santa_catarina']
 
 
 def adicionar_dados(x, uf):
-    if uf == 'Sao Paulo':
+    if uf.upper() == 'SP':
+        sao_paulo_collection.insert_one(x)
+    if uf.upper() == 'PR':
+        sao_paulo_collection.insert_one(x)    
+    if uf.upper() == 'RS':
+        sao_paulo_collection.insert_one(x)
+    if uf.upper() == 'SC':
         sao_paulo_collection.insert_one(x)
