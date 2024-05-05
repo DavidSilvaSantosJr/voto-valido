5612681da8b1843ec362b3d2ff366e80cf0a2baa
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-05-05 16:27:00 -0300

    Update buscas.py

--- buscas.py
+++ buscas.py
@@ -1,35 +1,18 @@
-import pymongo
 import conexao_mongo 
 
-
-
-def resumo_semana(local_a_consultar):
-    return list(conexao_mongo.colecao.find({"localizacao":local_a_consultar},
-                                                 {
-                                                      "local_de_envio": 0,
-                                                      "imagem":0,
-                                                      "_id":0
-                                                  }
-                                                  ))
-
-def adicionar_dados(x, uf):
-    if uf.upper() == "SÃO PAULO":
-        conexao_mongo.colecao_uf_insert_sp(x)
-
-def consultar_estado(x):
-    try:
-        consulta = conexao_mongo.dtb_completo.find_one({"Nome_UF": x})
-        return consulta["Nome_UF"]
-    except TypeError:
-        text = f"Verifique se o nome do estado foi digitado corretamente, e tente mais suma vez.\n"
-        f"Não encontrei nenhum estado chamado {x}!"
-        return text
-
-def consultar_cidade(x, uf):
-    try:
-        consulta = conexao_mongo.dtb_completo.find_one({"Nome_UF":uf, "Nome_Municipio":x})
-        return consulta["Nome_Municipio"]
-    except TypeError:
-        text = f"Verifique se o nome do estado municipio foi digitado corretamente, e tente mais suma vez.\n"
-        f"Não encontrei {x} em {uf}!"
-        return text
+def resumo_semana(uf, cidade):
+    uf = uf.upper()
+    if uf == "SP" or "SAO PAULO":
+        query_result =  list(conexao_mongo.sao_paulo_collection.find({
+            "cidade": cidade
+            },
+            {
+                "uf":1,
+                "descricao": 1
+            }))
+        
+    resposta_descricao = []
+    for i in query_result: 
+        resposta_descricao.append(i['descricao'])
+    print(resposta_descricao)
+    return resposta_descricao
