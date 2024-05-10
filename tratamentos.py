83e43c0731e4f97021cf2c831a485450458454eb
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-05-09 22:14:40 -0300

    Update tratamentos.py

--- tratamentos.py
+++ tratamentos.py
@@ -1,10 +1,6 @@
-import requests
-import unidecode
-import json
 from keys import key
 #import keys as keys
 api_key = key
-
 def texto_padrao(boas_vindas=None, agradecimento=None):
     if boas_vindas:
         texto = str(
@@ -14,8 +10,7 @@ def texto_padrao(boas_vindas=None, agradecimento=None):
             "basta enviar o link para alguém que esteja por lá.\n\n\n"
             "Siga as instruções, e seja bem vindo!\n"
             "caso eu pare,dê algum erro, ou pare de dar respostas, \n\n\nclique abaixo, em recomeçar que o processo será refeito\n\n"
-            "/recomeçar"
-
+            " /recomeçar "
         )
         return texto
     
@@ -29,6 +24,7 @@ def texto_padrao(boas_vindas=None, agradecimento=None):
 
             "Voce pode ver esse e outros problemas em [link plataforma], não esqueça de compartilhar esse problemas com outros moradores da redondeza,"
             "Isso ajuda a resolve-lo mais rápidamente."
+
         )
         return texto
 
@@ -37,12 +33,8 @@ def salvar_imagem(message):
     user_id = message.chat.id
     photo_id = message.photo[-1].file_id
     # Salvando o ID da foto no banco de dados
-    photo_data = {"user_id": user_id,"photo_id": photo_id}
-    return  {"user_id": user_id,"photo_id": photo_id}
-
-   
-
-            # Enviar mensagem de resposta
-
-#v = verificar_endereco("São Paulo", "Ourinhos", 'Parque Gabriela', 'Alameda Manoel Angelo minucci')
-#print(v)
+    photo_data = {"user_id": user_id,
+                  "photo_id": photo_id
+                  }
+    
+    return  photo_data
