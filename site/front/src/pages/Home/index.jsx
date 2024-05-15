8950cf8dab03f3d0eada0884ca2c5162a907eb4b
João Hugo Leda <joaohugoleda2@gmail.com>
2024-05-15 19:53:59 -0300

    add assets

--- site/front/src/pages/Home/index.jsx
+++ site/front/src/pages/Home/index.jsx
@@ -4,9 +4,8 @@ export default function Home(){
   return (
     <Content>
       <header className="search-box">
-        <select>
-          <option selected></option>
-          <option value="SP">SP</option>
+        <select>          
+          <option selected value="SP">SP</option>
           <option value="PR">PR</option>
           <option value="MG">MG</option>
         </select>
@@ -17,6 +16,36 @@ export default function Home(){
           </span>
         </div>
       </header>
+      <table class="table table-hover table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Cidade</th>
+            <th scope="col">Like</th>
+            <th scope="col">Deslike</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+        </tbody>
+      </table>
     </Content>
   )
 }
