e2626050a9a3858c1bd79702da76494a883425e5
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-05-22 08:35:24 -0300

    retorno dos locais com mais likes

--- buscas.py
+++ buscas.py
@@ -1,34 +1,40 @@
 import conexao_mongo 
 import time
 from funcoes import consultas_maps
+from unidecode import unidecode
+import re
 
-def resumo_semana(uf, cidade): #filtar os 10 com mais likes e comentarios, dos últimos 7 dias
-    uf = uf.upper()
-    if uf == "SAO PAULO":
-        query_result =  list(conexao_mongo.sao_paulo_collection.find({
-            "city": cidade
-            },
-            {   "_id":0,
-                "location":1,
-                "uf":1,
-                "descricao": 1,
-                "like": 1,
-                "bairro":1
+def resumo_semana(cidade): #filtar os 10 com mais likes e comentarios, dos últimos 7 dias
 
-            }).sort({
-                'like': -1
-            }).limit(10)
-            )
-        
-        print(query_result)
-        resposta_formatada = str(f"Em {cidade}, {uf.capitalize()}, esses são os problemas mais repercurtidos:\n")
-    
+    query_result =  list(conexao_mongo.uf_collection.find({
+        'city_formated': cidade
+    }
+        ,
+        {   "_id":0,
+            "location":1,
+            "uf":1,
+            "descricao": 1,
+            "like": 1,
+            "bairro":1
+
+        }).sort({
+            'like': -1
+        }).limit(10)
+        )
+    if len(query_result) <1:
+        return 'Ops!, parece que não há problemas registrados nessa cidade, verifique se tudo foi digitado corretamente, e tente novamente.'
+    resposta_formatada = str(f"Esses são os problemas mais repercurtidos da cidade:\n\n\n") ###exibir ceerrtoo###
+
     #forma para exibir na mensagem
     for i in query_result:
         print(i['location'])
-        resposta_formatada = f"\n{resposta_formatada} {i['descricao']}, no bairro {i['bairro']} com {i['like']} like(s); \n\n"
+        if i['like'] == 0:
+            resposta_formatada = f"\n{resposta_formatada} {i['descricao']}, no bairro {i['bairro']}, ainda com nenhum like; \n\n"
+        if i['like'] >=1:
+            resposta_formatada = f"\n{resposta_formatada} {i['descricao']}, no bairro {i['bairro']} com {i['like']} like(s); \n\n"
 
-    resposta_formatada += str(f"\nVocê pode ver os problemas em mapas, e entender melhor o que acontece em {cidade}, na nossa plataforma -> [LINK PLATAFORMA]!")
-    
-    
+
+    resposta_formatada += str(f"\nVocê também pode ver os problemas em mapas, e entender melhor o que acontece em {cidade}, futuramente em uma plataforma que estamos desenvolvendo!")
+
+
     return resposta_formatada
