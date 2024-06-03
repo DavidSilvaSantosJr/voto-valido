eef3c16c57446463174c6160aa3a16864f60f729
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-06-03 09:39:57 -0300

    Create problemas.py

---
+++ problemas.py
@@ -0,0 +1,71 @@
+dic_problemas = {
+    "Pavimentação": [
+        "Buracos no asfalto",
+        "Rachaduras e ondulações",
+        "Asfalto desgastado",
+        "Falta de sinalização horizontal(marcações no chão)",
+        "Sinalização viária apagada ou danificada"
+    ],
+    "Obras": [
+        "Desvios mal sinalizados",
+        "Faixas interditadas sem aviso prévio",
+        "Sinalização de obras incompleta ou inadequada",
+        "Atraso na conclusão das obras"
+    ],
+    "Drenagem": [
+        "Pontos de alagamento",
+        "Clogging de bueiros",
+        "Valetas entupidas"
+    ],
+    "Pontes e Passarelas": [
+        "Pontes danificadas",
+        "Passarelas em mau estado",
+        "Falta de iluminação em pontes e passarelas"
+    ],
+    "Transporte Público": [
+        "Falta de linhas de ônibus",
+        "Ônibus em mau estado",
+        "Frota insuficiente",
+        "Falta de segurança nos ônibus",
+        "Ponto de ônibus em mal estado"
+    ],
+    "Praças e Parques": [
+        "Falta de manutenção",
+        "Equipamentos de lazer danificados",
+        "Falta de iluminação",
+        "Presença de moradores de rua",
+        "Presença de usuários de drogas"
+    ],
+    "Calçadas": [
+        "Desníveis e buracos",
+        "Falta de rampas de acessibilidade",
+        "Calçadas estreitas",
+        "Ocupação por vendedores ambulantes"
+    ],
+    "Áreas Verdes": [
+        "Falta de cuidado e manutenção",
+        "Risco de incêndios",
+        "Invasão por construções irregulares",
+        "Árvores muito próximas de fiação elétrica"
+    ],
+    "Iluminação Pública": [
+        "Lâmpadas queimadas",
+        "Postes de iluminação danificados",
+        "Falta de iluminação em áreas periféricas"
+    ],
+    "Segurança Pública": [
+        "Falta de policiamento",
+        "Iluminação pública precária",
+        "Presença de pontos de drogas",
+        "Vandalismo"
+    ],
+    "Outros": [
+        "Falta de coleta de lixo",
+        "Rede de esgoto precária"
+    ]
+}
+
+topicos = []
+for k,v in dic_problemas.items():
+    topicos.append(k)
+#print (topicos)
