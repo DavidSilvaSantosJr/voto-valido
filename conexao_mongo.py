d7c93a95dbdbc94531d582c112dc87574130fa56
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-06-04 23:44:10 -0300

    Update conexao_mongo.py

--- conexao_mongo.py
+++ conexao_mongo.py
@@ -1,25 +1,23 @@
 import pymongo
 from keys import STRING_CONNECTION
 
-
 # Conectar ao servidor MongoDB (por padrão, o MongoDB roda em localhost, porta 27017)
 client = pymongo.MongoClient(STRING_CONNECTION)
 
 # banco de dados
 unioes_federativas = client["unioes_federativas"]
 # coleções
-sao_paulo_collection = unioes_federativas["sao_paulo"]
-parana_collection = unioes_federativas['parana']
-rio_grande_do_sul_collection = unioes_federativas['rio_grande_do_sul']
-santa_catarina_collection = unioes_federativas['santa_catarina']
+uf_collection = unioes_federativas["uf"]
+itens_incompleto_collection = unioes_federativas['itens_incompletos']
 
+def adicionar_dados(x): 
+    uf_collection.insert_one(x)
 
-def adicionar_dados(x, uf):
-    if uf.upper() == 'SP':
-        sao_paulo_collection.insert_one(x)
-    if uf.upper() == 'PR':
-        sao_paulo_collection.insert_one(x)    
-    if uf.upper() == 'RS':
-        sao_paulo_collection.insert_one(x)
-    if uf.upper() == 'SC':
-        sao_paulo_collection.insert_one(x)
+def adicionar_dados_incompletos(x): 
+    itens_incompleto_collection.insert_one(x)
+
+def atualizar(ObjectId, documento):
+    itens_incompleto_collection.update_one(
+        {"_id": ObjectId},    #filtro
+        {"$set": documento}   #novas informaçoes
+    )
