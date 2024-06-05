27ad6b37dee2e2cc23c33082f46fc64bdf4dcf11
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-06-04 23:44:28 -0300

    Update consultas_maps.py

--- consultas_maps.py
+++ consultas_maps.py
@@ -1,6 +1,7 @@
 import requests
-import unidecode
+from unidecode import unidecode
 import json
+from datetime import datetime
 from keys import key
 api_key = key
 
@@ -32,6 +33,7 @@ def salvar_latlong_endereco(endereco):
 
 
 def salvar_uf_bairro_cidade(coordenadas): #list
+    user_data = {}
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
     params = {
         "latlng": f"{coordenadas[0]},{coordenadas[1]}",
@@ -39,21 +41,36 @@ def salvar_uf_bairro_cidade(coordenadas): #list
     }
     response = requests.get(base_url, params=params)
     data = response.json()
-    if data['status'] == 'OK':
-        for result in data['results']:
-            for component in result['address_components']:
-                if 'administrative_area_level_1' in component['types']:
-                    uf = component['short_name']
+    try:
+        if data['status'] == 'OK':  #try -> UnboundLocalError: cannot access local variable 'cidade' where it is not associated with a value
+            for result in data['results']:
+                for component in result['address_components']:
+                    if 'administrative_area_level_1' in component['types']:
+                        uf = component['short_name']
 
-        for result in data['results']:
-            for component in result['address_components']:
-                if 'locality' in component['types']:
-                    cidade = component["long_name"]
-                    
-        for result in data['results']:
-            for component in result['address_components']:
-                if ('political' or 'sublocality' or 'sublocality_level_1') in component['types']:
-                    bairro = component['long_name']
-                    return [uf, cidade, bairro]
-                
-    return "pane no sistema"
+            for result in data['results']:
+                for component in result['address_components']:
+                    if 'locality' in component['types']:
+                        cidade = component["long_name"]
+                        
+            for result in data['results']:
+                for component in result['address_components']:
+                    if ('political' or 'sublocality' or 'sublocality_level_1') in component['types']:
+                        user_data['lat_long'] = [coordenadas[0], coordenadas[1]]
+                        bairro = component['long_name']
+                        user_data['uf'] = uf
+                        user_data['city'] = cidade
+                        user_data['bairro'] = bairro
+                        user_data['city_formated'] = unidecode(user_data['city'].lower())
+                        user_data['city_formated'] = user_data['city_formated'].replace("'", "")
+                        user_data['like'] = 0
+                        user_data['deslike'] = 0
+                        user_data['coments'] = {
+                        'time' : datetime.now(), #irá manipular formatos de data. Por isso a string
+                        'like' : int(),
+                        'deslike' : int(),
+                        'coment': str()
+                        }
+                        return user_data
+    except UnboundLocalError:
+        return 0
