60cf73197799c4b282dc568c8ca957dd425b6b27
David Silva Santos Júnior <55060834+DavidSilvaSantosJr@users.noreply.github.com>
2024-06-10 15:57:07 -0300

    Update problemas.py

--- problemas.py
+++ problemas.py
@@ -14,13 +14,15 @@ dic_problemas = {
     ],
     "Drenagem": [
         "Pontos de alagamento",
-        "Clogging de bueiros",
-        "Valetas entupidas"
+        "elevação/depressão de bueiros",
+        "Valetas entupidas",
+        "buerio com tampa quebrada"
     ],
     "Pontes e Passarelas": [
         "Pontes danificadas",
         "Passarelas em mau estado",
-        "Falta de iluminação em pontes e passarelas"
+        "Falta de iluminação em pontes e passarelas",
+        "risco de queda"
     ],
     "Transporte Público": [
         "Falta de linhas de ônibus",
@@ -34,7 +36,9 @@ dic_problemas = {
         "Equipamentos de lazer danificados",
         "Falta de iluminação",
         "Presença de moradores de rua",
-        "Presença de usuários de drogas"
+        "Presença de usuários de drogas",
+        "Mato alto",
+        "Árvores encostando em fios elétricos"
     ],
     "Calçadas": [
         "Desníveis e buracos",
@@ -51,17 +55,26 @@ dic_problemas = {
     "Iluminação Pública": [
         "Lâmpadas queimadas",
         "Postes de iluminação danificados",
-        "Falta de iluminação em áreas periféricas"
+        "Falta de iluminação em áreas periféricas",
+        "fios de poste/alta tensão furtados"
     ],
     "Segurança Pública": [
         "Falta de policiamento",
         "Iluminação pública precária",
         "Presença de pontos de drogas",
-        "Vandalismo"
+        "Vandalismo",
+        "Veículos executando manobras perigosas"
     ],
     "Outros": [
         "Falta de coleta de lixo",
         "Rede de esgoto precária"
+    ],
+    "Prédios do governo/prefeitura": [
+        "Mau estado",
+        "Funcionários mal educados",
+        "Falta de profissionais",
+        "Falta de professores",
+        "bebedouros/banheiros em má condições"
     ]
 }
 
@@ -69,3 +82,14 @@ topicos = []
 for k,v in dic_problemas.items():
     topicos.append(k)
 #print (topicos)
+
+## retorno botoes para tipo de atualizacao da cidade
+buscas = [
+    'meus_prob',
+    'mais_repercurtidos',
+    'busca_categoria'
+]
+
+estados = [
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "SC", "SE", "SP", "TO"
+]
